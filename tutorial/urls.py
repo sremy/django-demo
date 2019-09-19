0fc4086707398f778bedff0b3e3aeb2ed449fb30
sremy <seb.remy@gmail.com>
2019-09-20 00:58:32 +0200

    Add drf-yasg page

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,18 +14,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from tutorial.quickstart import views
 from rest_framework_swagger.views import get_swagger_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+
+drf_schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="API description",
+      terms_of_service="",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('rest-swagger/', get_swagger_view(title='Users API')),
+    re_path(r'^drf-yasg-swagger(?P<format>\.json|\.yaml)$', drf_schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('drf-yasg-swagger/', drf_schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('drf-yasg-redoc/', drf_schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
